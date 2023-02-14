335d1937c99c2139a8c497a1c40aea799f70cbd5
Ashley <Ashhamilton22888@gmail.com>
2023-02-14 11:43:44 -0500

    up to the farm

--- index.js
+++ index.js
@@ -10,10 +10,117 @@
 
 
 // I. Variables & Data Types
+// 1. How do we assign a value to a variable?  With the assignment operator... let or const, the name of the variable = value
+// 2. How do we change the value of a...
+// of a what?
+
+
 // A. Q + A
+
+// How do we assign a value to a variable? = the assignment operator
+
+// How do we change the value of a variable? 
+// let variable = value
+// variable = otherValue
+
+// How do we assign an existing variable to a new variable?
+// let variable = value;
+// let newVariable=value;
+
+
+// Remind me, what are declare, assign, and define?
+// declare- use let or const to make a variable
+// assign-give a variable a value
+// define-
+
+// What is pseudocoding and why should you do it?
+// pseudocoding is using words to say what you want your code to do. You should do it because it gives you a map of what you want your program to do before you write the actual code for it.
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it? 90%?
+
+
 // B. Strings
+// Create a variable called firstVariable
+// let firstVariable;
+
+
+// Assign it the value of the string "Hello World"
+// let firstVariable="Hello World";
+
+
+// Change the value of this variable to some number
+// firstVariable= 28;
+
+// Store the value of firstVariable in a new variable called secondVariable
+// let firstVariable="Hello World";
+// firstVariable= 28;
+// let secondVariable =firstVariable
+// console.log(secondVariable)
+
+// Change the value of secondVariableto any string.
+// let firstVariable="Hello World";
+//  firstVariable= 28;
+// let secondVariable =firstVariable
+// secondVariable="You are my sunshine"
+// console.log(secondVariable)
+
+
+
+
+// What is the value of firstVariable?
+//28
+
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+
+// let yourName="Ashley";
+// let expression = "Hello, my name is ";
+// let theString= expression + yourName;
+// console.log(theString)
+
+// ex: Hello, my name is Jean Valjean
+
+
 // C. Booleans
+
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true|| false);
+// console.log(false || false || false || false || false || true);
+// console.log(false === false)
+// console.log(e=== 'Kevin');
+// console.log(a !> b !> c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a === a !== d); // note: the answer is a simple arithmetic equation, not something "weird"
+
+// console.log(48 == '48');
+
+
+
+
 // D. The Farm
+// Declare a variable animal. Set it to be either "cow" or something else
+// Write code that will print out "mooooo" if the it is equal to cow
+// Change your code so that if the variable animalis anything other than a cow, it will print "Hey! You're not a cow."
+// Commit
+
+let animal="Snake";
+if(animal==="Cow"){
+    console.log("mooooo")
+} else if (animal!=="Cow"){
+    console.log("Hey! You're not a cow")
+}
+
+
+
 // E. Driver's Ed
 //_____________________________________________________________
 

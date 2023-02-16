acbdefc0e0f9fc833d885fb28eb6bd43b32a0f1b
Ashley <Ashhamilton22888@gmail.com>
2023-02-15 21:39:27 -0500

    im up to F.Loops

--- index.js
+++ index.js
@@ -453,28 +453,52 @@ const e = 'Kevin';
 // D.isVowel
 // Write a function isVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise. The vowel could be upper or lower case. Test your function on every vowel and make sure it's working. In general, when you write functions, take a minute to test them with different values to make sure they behave the way you want.
 
-// function isVowel(a)
-//     if(isVowel.toLowerCase() === "a"|| isVowel.toLowerCase() === "e"|| isVowel.toLowerCase() === "i"|| isVowel.toLowerCase() ===  "o"||isVowel.toLowerCase() ===  "u"){
-//         console.log(true)
+
+
+// function isVowel(a){
+//     a = a.toLowerCase()
+//     if(a.toLowerCase() ==="a" ||a.toLowerCase() === "e" || a.toLowerCase() ==="i" || a.toLowerCase() === "o" ||a.toLowerCase() === "u" ){
+//         return true
 //     }
+//     else {
+//         return false
+//     }
+// }
+// console.log(isVowel("a"))
 
 
-    // function isCharAVowel(char){
-        // char = char.toLowerCase()
-    //     if(char.toLowerCase() ==="a" ||char.toLowerCase() === "e" || char.toLowerCase() ==="i" ||char.toLowerCase() === "o" ||char.toLowerCase() === "u" ){
-    //         return true
-    //     }
-    //     else {
-    //         return false
-    //     }
-    // }
-    // console.log(isCharAVowel("A"))
+// E. getTwoLengths
+// Write a function getTwoLengths that accepts two parameters (strings). The function should return an array of numbers where each number is the length of the corresponding string.
+// function getTwoLengths(a,b){
+// return [a.length, b.length]
+// }
+// console.log(getTwoLengths("Ashley", "Hamilton"))
 
 
-// E.
-// F.
-// G.
-// H.
+
+// F. getMultipleLengths???????????
+// Write a function getMultipleLengths that accepts a single parameter as an argument: an array of strings. The function should return an array of numbers where each number is the length of the corresponding string.
+
+// function getMultipleLengths(arrayOfStrings) {
+//     return [arrayOfStrings.length]
+// }
+
+// console.log(getMultipleLengths["I love you", "Thank you", "I love you too"])
+
+
+// )
+
+// G.maxOfThree?????
+// Define a function maxOfThree that takes three numbers as arguments and returns the largest of them. If all numbers are the same, it doesn't matter which one is returned. If the two largest numbers are the same, one of them should be returned. Be sure to test it with larger values in each of the three locations.
+
+// function maxOfThree(a, b, c) {
+
+// }
+// console.log(maxOfThree(2, 6, 26))
+
+
+// H.printLongestWord
+// Write a function printLongestWordthat accepts a single argument, an array of strings. The method should return the longest word in the array. In case of a tie, the method should return the word that appears first in the array.
 //_____________________________________________________________
 
 
@@ -483,12 +507,88 @@ const e = 'Kevin';
 
 
 // V. Objects
-// A.
-// B.
-// C.
-// D.
-// E.
-// F.
+// A.Make a user object
+// Create an object called user.
+// Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchasedto an empty array []. Set the other values to whatever you would like.
+let user = {
+    name: "Asha",
+    email: "AshaNYC@gmail.com",
+    age: 30,
+    purchased: []
+}
+
+
+// B.Update the user
+// Our user has changed his or her email address. Without changing the original user object, update the email value to a new email address.
+user['email'] = "AshaNewYork@gmail.com"
+console.log(user)
+
+// Our user has had a birthday! Without changing the original user object, increment the age value using the postfix operator. Hint: age++
+console.log(user.age = user.age+1)
+
+// C.Adding keys and values
+// You have decided to add your user's location to the data that you want to collect.
+
+// Without changing the original user object, add a new key location to the object, and give it a value or some-or-other location (a string).
+user["location"]="NYC"
+console.log(user)
+
+// D.Shopaholic!
+// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+user["purchased"].push("carbohydrates")
+console.log(user)
+
+// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
+
+user["purchased"].push("peace of mind")
+console.log(user)
+
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+user["purchased"].push("Merino jodhpurs")
+console.log(user)
+
+
+// Console.log just the "Merino jodhpurs" from the purchased array.
+console.log(user["purchased"][2])
+
+// E. Object-within-object
+user.friend ={
+    name: "Yelhsa",
+    age: 32,
+    location: "NYC",
+    purchased:[]
+}
+
+// Console.log just the friend's name
+console.log(user.friend["name"])
+
+// Console.log just the friend's location
+console.log(user.friend["location"])
+
+// CHANGE the friend's age to 55
+user.friend['age'] = 55;
+console.log(user.friend)
+
+
+// The friend has purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchased array.
+user.friend["purchased"].push("The One Ring")
+console.log(user.friend)
+
+
+// The friend has purchased "A latte". Use .push()to add "A latte" to the friend's purchased array.
+user.friend["purchased"].push("A latte")
+console.log(user.friend)
+
+// Console.log just "A latte" from the friend's purchased array.
+console.log(user.friend["purchased"][1])
+
+
+
+
+// F.Loops
+// Write a for loop that iterates over the User's purchased array (NOT the friend's purchased array), and prints each element to the console.
+
+
 // G.
 //_____________________________________________________________
 

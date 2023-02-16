0f1797e0f1eae1b6ef41de8903371d840f034ed1
Ashley <Ashhamilton22888@gmail.com>
2023-02-16 15:46:00 -0500

    Yell at the Ninja Turtles answered

--- totalRecallDay2.js
+++ totalRecallDay2.js
@@ -31,27 +31,33 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant",  5000 , "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
 
-plantee.splice(2,1,5001);
-console.log(plantee);
+// plantee.splice(2,1,5001);
+// console.log(plantee);
 
-wolfy.splice(3,1, "Gotham City")
-console.log(wolfy)
+// wolfy.splice(3,1, "Gotham City")
+// console.log(wolfy)
 
-dart.push("Hawkins")
-console.log(dart)
+// dart.push("Hawkins")
+// console.log(dart)
 
-wolfy.splice(0,1,"Gameboy")
-console.log(wolfy)
+// wolfy.splice(0,1,"Gameboy")
+// console.log(wolfy)
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+// Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+
+let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for(let ninjaTurtle of ninjaTurtles){
+    console.log(ninjaTurtle.toUpperCase())
+}
 
 
 ////////////////////////////////
